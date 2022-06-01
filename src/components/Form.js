8a728f110cb7e04a92671237cd8180fef890e81a
Jonathan Aguilar <j.aguilar0390@gmail.com>
2022-06-01 12:11:48 -0400

    trying to fix button visibility still

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,22 +13,17 @@ function Form({setFeelingData}) {
     
     const buttonText = "Share your feelings with the world..."
 
-    // function handleFormChange(e) {
-    //     if(e.target.value.length > 0) {
-    //         setIsHidden(!isHidden)
-    //     } else {
-    //         setIsHidden(isHidden)
-    //     }
-    // }
+    
 
     function handleChange(e) {
-        if(e.target.value.length > 0) {
-            setIsHidden(!isHidden)
-        } else {
-            setIsHidden(isHidden)
-        };
+        // if(e.target.value.length > 0) {
+        //     setIsHidden(!isHidden)
+        // } else {
+        //     setIsHidden(isHidden)
+        // };
         // console.log(e.target);
         setForm((currentFormState) => ({...currentFormState, [e.target.name]: e.target.value}));
+       
     }
 
     function handleSubmit(e) {
@@ -40,19 +35,21 @@ function Form({setFeelingData}) {
             body: JSON.stringify(form),
         })
         .then(r => r.json())
-        .then(newContentData => setFeelingData((currentContent) => [...currentContent, newContentData]))
-        // must create a state in app or wherever contents/comments from db.json will be rendered
-    
-        // setForm(form);
+        .then(newContentData => setFeelingData((currentContent) => [...currentContent, newContentData]));
+        
         setForm(initialForm);
-      }
+    }
+    const handleHidden = ()=> {
+        if(form.content == "") {
+            setIsHidden(!isHidden)
+        }
+    }
     
     return (
         <form className="form" onSubmit={handleSubmit}>
-            <input name="name" placeholder="Name" type="text" value={form.name} onChange={handleChange}/>
-            <textarea name="content" placeholder="Write your comment here..." type="text" rows={5} value={form.content} onChange={handleChange}/>
-            {/* <input name="likes" placeholder="Likes" value={form.likes} onChange={handleChange}/> */}
-            <button type="submit" disabled={isHidden}>{buttonText}</button>
+            <input name="name" placeholder="Name" type="text" value={form.name} onChange={(handleChange)}/>
+            <textarea name="content" placeholder="Write your comment here..." type="text" rows={5} value={form.content} onChange={handleChange} onClick={handleHidden}/>
+           {isHidden ? (<button type="submit" disabled={true}>{buttonText}</button>) : (<button type="submit" disabled={false}>{buttonText}</button>)}
         </form>
     )
 }
